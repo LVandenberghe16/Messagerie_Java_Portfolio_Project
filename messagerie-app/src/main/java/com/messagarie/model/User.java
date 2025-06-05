6747b944646418b3adbefd9182485fc2a6664fa9
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-06-05 10:44:55 +0200

    [UPDATE] add the logic for the model and repository of the User class

--- messagerie-app/src/main/java/com/messagarie/model/User.java
+++ messagerie-app/src/main/java/com/messagarie/model/User.java
@@ -2,12 +2,59 @@ package com.messagerie.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 
 @Entity
 public class User {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
     private String email;
-    // getters/setters
+    private String password;
+
+
+    public User() {
+        // Default constructor for JPA
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
+
+
